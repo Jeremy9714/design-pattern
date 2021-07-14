1bd596e1ae80d7499f1feffe486ca2d45a972fa1
CYZ <1822550890@qq.com>
2021-07-14 15:29:13 +0800

    单一职责原则

--- DesignPattern/01_Principle/src/principle/singleResponsibility/SingleResponsibility1.java
+++ DesignPattern/01_Principle/src/principle/singleResponsibility/SingleResponsibility1.java
@@ -1,8 +1,34 @@
 package principle.singleResponsibility;
 
 /**
+ * 单一职责原则
+ *
  * @author Chenyang
  * @create 2021-07-14-14:50
  */
 public class SingleResponsibility1 {
+
+    public static void main(String[] args) {
+        Vehicle car = new Vehicle();
+        Vehicle plane = new Vehicle();
+        Vehicle ship = new Vehicle();
+        car.run("car");
+        plane.fly("plane");
+        ship.sail("ship");
+    }
+}
+
+class Vehicle {
+    public void run(String vehicle) {
+        System.out.println(vehicle + " is running");
+    }
+
+    public void fly(String vehicle) {
+        System.out.println(vehicle + " is flying");
+    }
+
+    public void sail(String vehicle) {
+        System.out.println(vehicle + " is sailing");
+    }
+
 }
